b6ce441bdab54d06267c688d57e3aa6c55116ec2
Vitor Santos Rodrigues <santos_vitao@hotmail.com>
2023-05-31 13:48:38 -0300

    :wrench: chore: add prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -1,3 +1,8 @@
 module.exports = {
+  singleQuote: true,
+  arrowParens: 'always',
+  trailingComma: 'none',
+  printWidth: 100,
+  tabWidth: 2,
   plugins: [require('prettier-plugin-tailwindcss')],
 }
