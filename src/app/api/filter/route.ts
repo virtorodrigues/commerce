5167909ef81d51c8b0bc94effa861937bed06406
Vitor Santos Rodrigues <santos_vitao@hotmail.com>
2023-06-17 22:50:19 -0300

    fix: try fix api filter

--- src/app/api/filter/route.ts
+++ src/app/api/filter/route.ts
@@ -1,11 +1,5 @@
-import { NextRequest, NextResponse } from 'next/server'
+import { NextRequest } from 'next/server'
 
 export async function GET(request: NextRequest) {
-  const { searchParams } = new URL(request.url)
-  const condition = searchParams.get('condition') as string
-
-  const redirectURL = new URL('/', request.url)
-  redirectURL.searchParams.set('condition', condition)
-
-  return NextResponse.redirect(redirectURL)
+  console.log('dsadas')
 }
