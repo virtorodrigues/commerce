babd01dfc5b32d09e5c42e271fa34efb61cc1807
Vitor Santos Rodrigues <santos_vitao@hotmail.com>
2023-06-17 22:47:41 -0300

    fix: try fix api filter

--- src/app/api/filter/route.ts
+++ src/app/api/filter/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server'
 
-export function GET(request: NextRequest) {
+export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url)
   const condition = searchParams.get('condition') as string
 
