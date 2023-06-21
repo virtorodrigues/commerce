e3e5273f382dff9641c06975cc9d416ef9739eb9
Vitor Santos Rodrigues <santos_vitao@hotmail.com>
2023-06-21 17:45:42 -0300

    feat: add open graph metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,8 +8,23 @@ const roboto = Roboto({
 })
 
 export const metadata = {
-  title: 'Compre o seu controle',
+  title: 'Marcelo Controles',
   description: 'Compre seu controle favorito e cote seu serviço.',
+  openGraph: {
+    title: 'Marcelo Controles',
+    description: 'Compre seu controle favorito e cote seu serviço.',
+    url: process.env.NEXT_PUBLIC_API_URL_BASE,
+    siteName: 'Marcelo Controles',
+    images: [
+      {
+        url: './icon.png',
+        width: 800,
+        height: 600,
+      },
+    ],
+    locale: 'pt-BR',
+    type: 'website',
+  },
 }
 
 export default function RootLayout({
@@ -18,7 +33,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
       <body className={roboto.className}>{children}</body>
     </html>
   )
