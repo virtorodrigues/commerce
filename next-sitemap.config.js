ef61e97802f259cd7be4ccbec6ed661f7d20a66f
Vitor Santos Rodrigues <santos_vitao@hotmail.com>
2023-06-21 18:16:02 -0300

    feat: add sitemaps

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://marcelocontroles.vercel.app',
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+}
